67ed88b9cdfb56f072fe275d03df344018aa361b
Joshua King <joshuaking85@gmail.com>
2020-05-24 19:23:32 -0400

    Removed / cleaned up project. Added methods to Map and Game.cs files

--- Game.cs
+++ Game.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Game
     {
-        public void Main()
+        public static void Main()
         {
-            Tower tower = new Tower();
+            Map map = new Map(8, 5);
+            
+            Point point = new Point(4, 2);
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+            
+            point = new Point(8, 5);
+            isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
         }
     }
 }
--- Tower.cs
+++ Tower.cs
@@ -1,5 +1,3 @@
-﻿using System;
-
 namespace TreehouseDefense
 {
     class Tower
