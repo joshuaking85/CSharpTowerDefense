6b10bd7c3306d34f4682bc88ad5d2d9252fc59bf
Joshua King <joshuaking85@gmail.com>
2020-05-26 23:17:25 -0400

    Added Tower and invader arrays to the game.cs file

--- Game.cs
+++ Game.cs
@@ -12,18 +12,40 @@ namespace TreehouseDefense
             {
                 Path path = new Path(
                     new [] {
-                    new MapLocation(0, 2, map), 
-                    new MapLocation(1, 2, map),
-                    new MapLocation(2, 2, map),
-                    new MapLocation(3, 2, map),
-                    new MapLocation(4, 2, map),
-                    new MapLocation(5, 2, map),
-                    new MapLocation(6, 2, map),
-                    new MapLocation(7, 2, map)
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map)
                     }
                 );
-            } 
-            
+                
+                Invader[] invaders = 
+                {
+                    new Invader(path),
+                    new Invader(path),
+                    new Invader(path),
+                    new Invader(path)
+                };
+                
+                Tower[] towers = {
+                    new Tower(new MapLocation(1, 3, map)),
+                    new Tower(new MapLocation(3, 3, map)),
+                    new Tower(new MapLocation(5, 3, map))
+                };
+                
+                Level leve1 = new Level(invaders)
+                {
+                    Towers = towers
+                };
+                
+                bool playerWon = leve1.Play();
+                
+                Console.WriteLine("Player " + (playerWon? "won" : "lost"));
+            }
             catch(OutOfBoundsException ex)
             {
                 Console.WriteLine(ex.Message);
@@ -32,7 +54,8 @@ namespace TreehouseDefense
             {
                 Console.WriteLine("Unhandled TreehouseDefenseException");
             }
-            catch(Exception ex) {
+            catch(Exception ex)
+            {
                 Console.WriteLine("Unhandled Exception: " + ex);
             }
         }
