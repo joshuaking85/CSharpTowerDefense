5db177997c083d9a5b498c0b63260e9b3fd29bd1
Joshua King <joshuaking85@gmail.com>
2020-06-02 14:36:08 -0400

    Created StrongInvader class

--- Game.cs
+++ Game.cs
@@ -27,7 +27,7 @@ namespace TreehouseDefense
                 {
                     new ShieldedInvader(path),
                     new FastInvader(path),
-                    new Invader(path),
+                    new StrongInvader(path),
                     new Invader(path)
                 };
                 
