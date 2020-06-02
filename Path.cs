8b49ba3f77ec11bd93087f5e443f8a4ec313d25c
Joshua King <joshuaking85@gmail.com>
2020-06-02 16:07:25 -0400

    Created Equals method on the Point Class to check if the object is a Point then checks whether the object contains the same coordinates

--- Path.cs
+++ Path.cs
@@ -12,5 +12,16 @@ namespace TreehouseDefense
         {
             return (pathStep < _path.Length) ? _path[pathStep] : null;
         }
+        public bool IsOnPath(MapLocation location)
+        {
+            foreach(var pathLocation in _path)
+            {
+                if(location.Equals(pathLocation))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- Point.cs
+++ Point.cs
@@ -15,16 +15,17 @@ namespace TreehouseDefense
         {
             return X + ", " + Y;
         }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Point))
+            {
+                return false;
+            }
+            Point that = obj as Point;
+            return this.X == that.X && this.Y == that.Y;
+        }
         public int DistanceTo(int x, int y)
         {
-            /* int xDiff = X - x;
-            int yDiff = Y - y;
-
-            int xDiffSquared = xDiff * xDiff;
-            int yDiffSquared = yDiff * yDiff;
-
-            return (int)Math.Sqrt(xDiffSquared + yDiffSquared); */
-
             return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
         }
 
