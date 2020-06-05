8d06e99e5adaed5df4c44bbcf4c8447d4400ab78
Joshua King <joshuaking85@gmail.com>
2020-06-04 21:46:06 -0400

    Modified IInvader.cs class

--- IInvader.cs
+++ IInvader.cs
@@ -1,13 +1,19 @@
 namespace TreehouseDefense
 {
-    interface IInvader
+    interface IMappable
     {
         MapLocation Location {get;}
+    }
+    interface IMovable
+    {
+        void Move();
+    }
+    interface IInvader : IMappable, IMovable
+    {
         int Health {get;}
         bool HasScored {get;}
         bool IsNeutralized {get;}
         bool isActive {get;}
-        void Move();
         void DecreaseHealth(int factor);
     }
 }
